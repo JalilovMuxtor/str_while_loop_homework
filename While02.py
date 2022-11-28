4404d47179c9a2b0f85c86ce1cb8b7896bfda7fc
Muxtor <jalilovmuxtor203@gmail.com>
2022-11-29 00:07:49 +0500

    02

--- While02.py
+++ While02.py
@@ -6,4 +6,13 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    y=0
+    while i<len(s):
+
+        if s[i].isalpha():
+            
+            y+=1
+        i+=1
+    return y
+print(main('python 2022'))
