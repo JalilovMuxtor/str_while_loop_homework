55e4192fdad72ca1411d78793606eaaf6abd0f61
Muxtor <jalilovmuxtor203@gmail.com>
2022-11-30 21:33:42 +0500

    06

--- While03.py
+++ While03.py
@@ -10,10 +10,10 @@ def main(s):
     z=0
     while i<len(s):
 
-        if s[i].isalpha():
+        if not s[i].isalpha() and not s[i].isdigit():
 
             z+=1
         i+=1
 
     return z
-print(main('@com.uz#'))
+print(main('@com.uz@#'))
